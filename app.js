6f881727a4410328d3e068100c98b2d8fd84b718
Charles Rooney <crooney36@gmail.com>
2023-02-27 14:23:38 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  </head>
+  <body>
+  <ul>
+  ${posts.map((post) => `<li>${post.title}</li>`)}
+  </ul>
+  </body>
+  </html>
+  `;
+  res.send(html);
+});
 
 const PORT = 1337;
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
